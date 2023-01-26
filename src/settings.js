bff0560ca4f81656dea2f535e2d2168106a76206
Huzaifa Rehman <60300174+Huzaifa246@users.noreply.github.com>
2023-01-25 23:02:59 -0800

    Add files via upload

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "f7b03d2790e4450998e7d665131df58b";
 const token =
-  "007eJxTYHivEPlFacF/lY5DQkdWOXPu+ran7WJivjbDa9eY9K/ienkKDGnmSQbGKUbmlgapJiamBpaWFqnmKWZmpobGhilpphZJHguOJjcEMjJsk17ExMgAgSA+C0NuYmYeAwMAS9YfCg==";
+  "007eJxTYKiL3LF9zwfJeS6MF5VKvknPP1omXNRxul6hbM0khrw982cpMKSZJxkYpxiZWxqkmpiYGlhaWqSap5iZmRoaG6akmVokVahcSm4IZGS4dEqchZEBAkF8FobcxMw8BgYASKIfKw==";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
